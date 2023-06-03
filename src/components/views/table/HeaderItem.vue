115630e95198ae40c979c77fc50eb62ca5de0278
Sidachenko Michael <michaellux@yandex.ua>
2023-06-03 19:58:54 +0300

    add asc/desc sorting

--- src/components/views/table/HeaderItem.vue
+++ src/components/views/table/HeaderItem.vue
@@ -5,6 +5,12 @@
 <script>
 export default {
   name: 'HeaderItem',
+  data() {
+    return {
+      rootUserListSortedByThisHeaderLabel: false,
+      sortOrderBy: 'asc',
+    };
+  },
   props: {
     label: {
       type: String,
@@ -21,6 +27,10 @@ export default {
   },
   methods: {
     sortUsers(label) {
+      if (this.rootUserListSortedByThisHeaderLabel) {
+        this.sortOrderBy = this.sortOrderBy === 'asc' ? 'desc' : 'asc';
+      }
+
       const rootUserList = this.$root.$children[0].$children[0];
       const allUsers = rootUserList.users;
       if (label === 'name') {
@@ -28,6 +38,8 @@ export default {
       } else if (label === 'phone') {
         this.sortByPhone(allUsers, label);
       }
+
+      this.rootUserListSortedByThisHeaderLabel = true;
     },
     // https://medium.com/swlh/filtering-sorting-and-searching-in-arrays-with-vue-js-f60951c040fc
     sortByName(allUsers, label) {
@@ -35,10 +47,10 @@ export default {
         const fa = a[label].toLowerCase();
         const fb = b[label].toLowerCase();
         if (fa < fb) {
-          return -1;
+          return this.sortOrderBy === 'asc' ? -1 : 1;
         }
         if (fa > fb) {
-          return 1;
+          return this.sortOrderBy === 'asc' ? 1 : -1;
         }
         return 0;
       });
@@ -48,10 +60,10 @@ export default {
         const fa = a[label].replace(/\D/g, '');
         const fb = b[label].replace(/\D/g, '');
         if (fa < fb) {
-          return -1;
+          return this.sortOrderBy === 'asc' ? -1 : 1;
         }
         if (fa > fb) {
-          return 1;
+          return this.sortOrderBy === 'asc' ? 1 : -1;
         }
         return 0;
       });
